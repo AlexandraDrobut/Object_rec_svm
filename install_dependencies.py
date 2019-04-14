f55d49459a934fa95ac8bab31b1c087c4b0c0e7f
Alexandra Drobut <Alexandra.Drobut@riverbed.com>
2019-04-14 21:19:49 +0300

    Easy install of dependencies of the project

---
+++ install_dependencies.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get install python-pyqtgraph
+sudo apt-get install python3-pyside
+sudo -H pip3 install pyqtgraph
+sudo -H pip3 install cv2
+sudo -H pip3 install wget
+sudo apt-get install python3-matplotlib
+sudo -H pip3 install pyside
+sudo -H pip3 install pyqt5
+sudo -H pip3 install pandas
+
+
+
+
+
